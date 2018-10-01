c41e5f738c7c7e6d10567b835708763e7a32e731
Volodymyr Dedenko <dedenkof@ukr.net>
2018-10-01 18:04:57 +0300

    Breakdown of modules tasks gulpfile.js to build-assets-css-app.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,11 @@
 'use strict';
-// Подключаем необходимые модули для работы
-var gulp = require('gulp'),
-// gulp-load-plugins позволяет проще подключать плагины
-gp = require('gulp-load-plugins') ();
+global.$ = {
+    gulp: require('gulp'),
+    gp: require('gulp-load-plugins')()
+};
 
-// Gulp это обьект
-// task - метод
-gulp.task('sass', function(){
-// gulp возьми ./source/style/app.scss
-    return gulp.src('./source/style/app.scss')
-    // pipe это методы gulp которые совершают действия с взятыми нами файлами (типа труба)
-    // вызывает плагин sourcemaps и в начале трубы иницыализируем этот процес
-        .pipe(gp.sourcemaps.init())
-        // Запускаем sass для компиляции
-        .pipe(gp.sass())
-        // Если ошибка то сообщение с заголовком Style
-        .on('error', gp.notify.onError({
-            title : 'Style'
-        }))
-        // запускаем автопрефикс для браузеров
-        .pipe(gp.autoprefixer({
-            browsers: [
-                'last 3 version',
-                '> 1%',
-                'ie 8',
-                'ie 9',
-                'Opera 12.1'
-            ]
-        }))
-        // После єтих действий записываем в соурсмап
-        .pipe(gp.sourcemaps.write())
-        // после ложим в папку назначения
-        .pipe(gulp.dest('build/assets/css'))
-});
+// реквайрим модуль sass.js
+// Делаем вызов (регистрируем таск), чтобы иметь к нему доступ
+require('./gulp/tasks/sass.js')();
 
-
-/*
-Если таск не дефолтный то вызываем gulp sass по названию задачи
-Если дефолтный то просто вызываем gulp */
+$.gulp.task('sass');
